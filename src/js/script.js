9a24ff128694d470554e3c4505a3938580c4795e
Jonas Krogh Fischer <jonasfiskekrogh@hotmail.com>
2022-12-12 12:15:56 +0100

    burger-menu-iterations

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,8 @@
-document.querySelector("#icon123").addEventListener("click", myFunction);
+const toggleButton = document.getElementsByClassName("toggle-menu")[0];
+const nav2 = document.getElementById("full_screen_nav");
+const mainHeader = document.querySelector("header");
 
-function myFunction() {
-  console.log("btn clicked");
-}
+toggleButton.addEventListener("click", () => {
+  console.log("menu clicked");
+  nav2.classList.toggle("open");
+});
