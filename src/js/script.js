db9991325459a78380ae17aaabbc083f039cab3a
Jonas Krogh Fischer <jonasfiskekrogh@hotmail.com>
2022-12-12 14:26:07 +0100

    menu done næsten

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,23 @@
 const toggleButton = document.getElementsByClassName("toggle-menu")[0];
 const nav2 = document.getElementById("full_screen_nav");
 const mainHeader = document.querySelector("header");
+const logo = document.querySelector(".logo");
 
 toggleButton.addEventListener("click", () => {
   console.log("menu clicked");
   nav2.classList.toggle("open");
+  hideLogo();
 });
+
+window.addEventListener("resize", hideLogo);
+
+function hideLogo() {
+  if (
+    nav2.classList.contains("open") ||
+    window.matchMedia("(max-width: 600px)").matches
+  ) {
+    logo.style.display = "none";
+  } else {
+    logo.style.display = "block";
+  }
+}
