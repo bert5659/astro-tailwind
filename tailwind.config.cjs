07051d0c8135396c4ea0a5fea8a54741a1b32a06
Bertil-Bjoernboel <bert5659@stud.kea.dkgit config --global user.email Bert5659@stud.kea.dk>
2022-12-13 10:50:50 +0100

    Ny kurv plus cookiebox

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -84,9 +84,6 @@ module.exports = {
     borderRadius: {
       cornerRound: "10px",
     },
-    boxShadow: {
-      shadowBox: "0 4px 4px 14px rgba(0, 0, 0, 0.1)",
-    },
   },
   plugins: [],
 };
